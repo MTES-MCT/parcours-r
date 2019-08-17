c837ea17b63cc70bcfbdfdce05c1af168653d1f3
Stéphane Trainel <stephane.trainel@gmail.com>
2019-08-18 01:51:53 +0200

    update buikd.sh with m3 module

--- _build.sh
+++ _build.sh
@@ -14,7 +14,7 @@ cd ../..
 
 # Compilation du module 3
 cd ./Supports_formations/m3_stats_desc/
-#Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 cd ../..
 
 # Compilation du module 4

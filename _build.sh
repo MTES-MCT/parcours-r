61bab5ec2ab9a6a194c80aecb3fd2421d5e46179
Stéphane Trainel <stephane.trainel@gmail.com>
2019-08-17 17:11:07 +0200

    Update _build.sh

--- _build.sh
+++ _build.sh
@@ -2,15 +2,24 @@
 
 set -ev
 
+# Compilation du module 1
 cd ./Supports_formations/m1_socle/
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+#Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 cd ../..
 
-  
+# Compilation du module 2
 cd ./Supports_formations/m2_preparation_donnees/
+#Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+cd ../..
+
+# Compilation du module 3
+cd ./Supports_formations/m3_stats_desc/
+#Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+cd ../..
+
+# Compilation du module 4
+cd ./Supports_formations/m4_analyse_donnees/
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 cd ../..
 
-cd ./Supports_formations/m3_stats_desc/
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-cd ../..
+

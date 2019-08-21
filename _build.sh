91a9c7a5679994c1a5960cce2b2954d2ae3738f3
Romain Lesur <romain.lesur@gmail.com>
2019-08-22 01:22:54 +0200

    utilise make dans le script de build

--- _build.sh
+++ _build.sh
@@ -2,27 +2,7 @@
 
 set -ev
 
-# Compilation du module 1
-cd ./Supports_formations/m1_socle/
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-cd ../..
+make all MODULES=m1_socle m2_preparation_donnees m3_stats_desc m4_analyse_donnees m5_valorisation_des_donnees
 
-# Compilation du module 2
-cd ./Supports_formations/m2_preparation_donnees/
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-cd ../..
-
-# Compilation du module 3
-cd ./Supports_formations/m3_stats_desc/
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-cd ../..
-
-# Compilation du module 4
-cd ./Supports_formations/m4_analyse_donnees/
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-cd ../..
-
-# Compilation du module 5
-cd ./Supports_formations/m5_valorisation_des_donnees/
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-cd ../..
+# pour générer l'ensemble des modules y compris mx_travail_collaboratif :
+# make all
--- _build_only_m3.sh
+++ _build_only_m3.sh
@@ -3,7 +3,4 @@
 set -ev
 
 # Compilation du module 3
-cd ./Supports_formations/m3_stats_desc/
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-cd ../..
-
+make m3_stats_desc

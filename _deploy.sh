affa67eaa052fb693f78f44be349e88405bf72d7
Romain Lesur <romain.lesur@gmail.com>
2019-08-22 03:35:05 +0200

    revert https://github.com/MTES-MCT/parcours-r/pull/9/commits/9eb486865535ccb2aac08a76883645a5cd833261

--- _deploy.sh
+++ _deploy.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
-
 git config --global user.email "stephane.trainel@gmail.com"
 git config --global user.name "Stéphane Trainel"
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git output

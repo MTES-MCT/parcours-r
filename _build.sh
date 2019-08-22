9dfa0a9d4dbd776a9a192f32097b0f725697b78a
Romain Lesur <romain.lesur@gmail.com>
2019-08-22 02:52:31 +0200

    quote la liste les modules

--- _build.sh
+++ _build.sh
@@ -2,7 +2,7 @@
 
 set -ev
 
-make all MODULES=m1_socle m3_stats_desc m4_analyse_donnees m5_valorisation_des_donnees
+make all MODULES="m1_socle m2_preparation_donnees m3_stats_desc m4_analyse_donnees m5_valorisation_des_donnees"
 
 # pour générer l'ensemble des modules y compris mx_travail_collaboratif :
 # make all

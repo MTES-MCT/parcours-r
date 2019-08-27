94882ff871d77f337218fdebd0a67630e5869853
Stéphane Trainel <stephane.trainel@gmail.com>
2019-08-27 10:57:50 +0200

    update deploy.sh with comments

--- _deploy.sh
+++ _deploy.sh
@@ -6,8 +6,10 @@ git config --global user.email "stephane.trainel@gmail.com"
 git config --global user.name "Stéphane Trainel"
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git output
 
+# Suppresion des fichiers pour maintenir un espace le plus propre possible
 rm -fr ./output/*
 
+# Copie des fichiers par module
 cp -r ./Supports_formations/m1_socle/_book ./output/
 mv ./output/_book ./output/m1
 cp -r ./Supports_formations/m1_socle/_bookdown_files ./output/m1/
@@ -28,9 +30,10 @@ cp -r ./Supports_formations/m5_valorisation_des_donnees/_book ./output/
 mv ./output/_book ./output/m5
 cp -r ./Supports_formations/m5_valorisation_des_donnees/_bookdown_files ./output/m5/
 
-
+# Copie des fichiers communs
 cp -r ./documents ./assets ./semantic index.html ./output/
 
+# Publication des fichiers sur la branche "gh-pages" de GitHub
 cd output
 git add --all -f ./*
 git commit --allow-empty -m"Update site - build by travis-ci (#${TRAVIS_BUILD_NUMBER})"

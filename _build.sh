cbc624e24a7bb17b66703828683d3fdfecac3b1a
Romain LESUR <RLesur@users.noreply.github.com>
2019-08-28 15:48:44 +0200

    Render book maybe (#12)
    * add _render_book_maybe script and docopt as dependency
    
    * make the _render_book_maybe script executable
    
    * use _render_book_maybe to build the bookdown books

--- _build.sh
+++ _build.sh
@@ -3,26 +3,16 @@
 set -ev
 
 # Compilation du module 1
-cd ./Supports_formations/m1_socle/
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-cd ../..
+./_render_book_maybe.R -f bookdown::gitbook  ./Supports_formations/m1_socle/
 
 # Compilation du module 2
-cd ./Supports_formations/m2_preparation_donnees/
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-cd ../..
+./_render_book_maybe.R -f bookdown::gitbook  ./Supports_formations/m2_preparation_donnees/
 
 # Compilation du module 3
-cd ./Supports_formations/m3_stats_desc/
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-cd ../..
+./_render_book_maybe.R -f bookdown::gitbook  ./Supports_formations/m3_stats_desc/
 
 # Compilation du module 4
-cd ./Supports_formations/m4_analyse_donnees/
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-cd ../..
+./_render_book_maybe.R -f bookdown::gitbook  ./Supports_formations/m4_analyse_donnees/
 
 # Compilation du module 5
-cd ./Supports_formations/m5_valorisation_des_donnees/
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-cd ../..
+./_render_book_maybe.R -f bookdown::gitbook  ./Supports_formations/m5_valorisation_des_donnees/

8daf5d6a87b77df42582ce81942408253aa0b4e3
Stéphane Trainel <stephane.trainel@gmail.com>
2019-08-29 22:35:19 +0200

    build add epub for m1

--- Supports_formations/m1_socle/m1_admin.R
+++ Supports_formations/m1_socle/m1_admin.R
@@ -1,3 +1,7 @@
 # Commande pour générer à la main le book du module 1
 
 bookdown::render_book("index.Rmd", "bookdown::gitbook")
+bookdown::render_book("index.Rmd", "bookdown::epub_book")
+
+# Erreur à la compilation
+# bookdown::render_book("index.Rmd", "bookdown::pdf_book")

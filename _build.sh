c3246928eeee274cbd9053c8de08a6ba14dae415
Stéphane Trainel <stephane.trainel@gmail.com>
2019-09-25 23:35:09 +0200

    ajout bouton download pdf/epub pour m3

--- _build.sh
+++ _build.sh
@@ -9,7 +9,7 @@ set -ev
 ./_render_book_maybe.R -f all  ./Supports_formations/m2_preparation_donnees/
 
 # Compilation du module 3
-./_render_book_maybe.R -f bookdown::gitbook  ./Supports_formations/m3_stats_desc/
+./_render_book_maybe.R -f all  ./Supports_formations/m3_stats_desc/
 
 # Compilation du module 4
 ./_render_book_maybe.R -f bookdown::gitbook  ./Supports_formations/m4_analyse_donnees/
